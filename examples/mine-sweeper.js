98967017af0240a8420e27ac011e9fdb6400edbe
erickmerchant <erickmerchant@gmail.com>
2024-03-19 10:38:05 -0400

    Add examples

---
+++ examples/mine-sweeper.js
@@ -0,0 +1,290 @@
+import {html, render, classes} from "../lib.js";
+
+const PLAY_STATES = {
+	PLAYING: 0,
+	LOST: 1,
+	WON: 2,
+};
+
+export default function mineSweeper({height, width, mineCount}, target) {
+	let playState = PLAY_STATES.PLAYING;
+	let time = 0;
+	let flagCount = mineCount;
+	let boardMap = new Map();
+	let startTime = null;
+	let timeInterval = null;
+	let hiddenCount = height * width;
+	let adjacentMap = new Map();
+
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			boardMap.set(`${x} ${y}`, {
+				x,
+				y,
+				isFlagged: false,
+				isRevealed: false,
+				isArmed: false,
+				armedAdjacentCount: 0,
+			});
+		}
+	}
+
+	update();
+
+	function update() {
+		render(
+			html`
+				<div class="info-panel">
+					<div class="flag-count">
+						<div>🚩</div>
+						${flagCount}
+					</div>
+					<div aria-live="polite">${["", "💀", "🎉"][playState]}</div>
+					<div class="time">
+						<div>⏱️</div>
+						${time}
+					</div>
+				</div>
+				<div
+					class="board"
+					aria-row-count="${height}"
+					aria-col-count="${width}"
+					role="grid">
+					${range(height).map(
+						(y) => html`
+							<div role="row">
+								${range(width).map((x) => {
+									let square = boardMap.get(`${x} ${y}`);
+
+									return html`
+											<div role="gridcell" aria-rowindex="${y + 1}" aria-colindex="${x + 1}">
+												<button
+													aria-label="${square.isRevealed ? null : "Hidden"}"
+													type="button"
+													style="--column: ${x + 1}; --row: ${y + 1}"
+													class="${classes({
+														revealed: square.isRevealed,
+														flagged: square.isFlagged,
+														...range(8).reduce((cls, i) => {
+															cls[`armed-adjacent-count--${i}`] =
+																square.armedAdjacentCount === i;
+
+															return cls;
+														}, {}),
+													})}"
+													onclick=${revealSquare(x, y)}
+													oncontextmenu=${toggleFlag(x, y)}
+													onkeydown=${moveFocus(x, y)}>
+													${
+														!square.isRevealed
+															? square.isFlagged
+																? "🚩"
+																: ""
+															: square.isFlagged && !square.isArmed
+															? "❌"
+															: square.isArmed
+															? "💥"
+															: square.armedAdjacentCount || ""
+													}
+											</div>
+										`;
+								})}
+							</div>
+						`
+					)}
+				</div>
+			`,
+			target
+		);
+	}
+
+	function updateTime() {
+		time = Math.floor((Date.now() - startTime) / 1000);
+
+		update();
+	}
+
+	function revealSquare(x, y) {
+		return () => {
+			let square = boardMap.get(`${x} ${y}`);
+
+			if (playState !== PLAY_STATES.PLAYING) {
+				return;
+			}
+
+			if (hiddenCount === height * width) {
+				let armed = [...boardMap.values()].map((s) => ({
+					square: s,
+					order: s === square ? 2 : Math.random(),
+				}));
+
+				armed.sort((a, b) => a.order - b.order);
+
+				armed = armed.splice(0, mineCount);
+
+				for (let {square} of armed) {
+					square.isArmed = true;
+
+					for (let adjacent of getAdjacent(square.x, square.y)) {
+						adjacent.armedAdjacentCount += 1;
+					}
+				}
+
+				playState = PLAY_STATES.PLAYING;
+
+				startTime = Date.now();
+				timeInterval = setInterval(updateTime, 250);
+			}
+
+			if (!square.isFlagged) {
+				square.isRevealed = true;
+
+				hiddenCount -= 1;
+
+				if (square.isArmed) {
+					playState = PLAY_STATES.LOST;
+
+					clearInterval(timeInterval);
+
+					for (let square of boardMap.values()) {
+						if (!(square.isFlagged && square.isArmed)) {
+							square.isRevealed = true;
+						}
+					}
+				} else {
+					if (!square.isFlagged && square.armedAdjacentCount === 0) {
+						let current = getAdjacent(x, y);
+
+						do {
+							let next = [];
+
+							for (let square of current) {
+								if (!square || square.isRevealed) {
+									continue;
+								}
+
+								if (!square?.isArmed && !square?.isFlagged) {
+									square.isRevealed = true;
+
+									hiddenCount -= 1;
+
+									if (square.armedAdjacentCount === 0) {
+										next.push(...getAdjacent(square.x, square.y));
+									}
+								}
+							}
+
+							current = next;
+						} while (current.length > 0);
+					}
+
+					if (hiddenCount === mineCount) {
+						playState = PLAY_STATES.WON;
+
+						clearInterval(timeInterval);
+					}
+				}
+			}
+
+			update();
+		};
+	}
+
+	function toggleFlag(x, y) {
+		return (e) => {
+			let square = boardMap.get(`${x} ${y}`);
+
+			e.preventDefault();
+
+			if (!square.isRevealed) {
+				square.isFlagged = !square.isFlagged;
+
+				flagCount += square.isFlagged ? -1 : 1;
+			}
+
+			update();
+		};
+	}
+
+	function moveFocus(x, y) {
+		return (e) => {
+			let keys = {
+				ArrowUp: [[x, y - 1]],
+				ArrowDown: [[x, y + 1]],
+				ArrowLeft: [
+					[x - 1, y],
+					[width - 1, y - 1],
+				],
+				ArrowRight: [
+					[x + 1, y],
+					[0, y + 1],
+				],
+			};
+
+			for (let [x, y] of keys?.[e.key] ?? []) {
+				let square = target.querySelector(
+					`[role="row"]:nth-child(${y + 1}) [role="gridcell"]:nth-child(${
+						x + 1
+					}) button`
+				);
+
+				if (square) {
+					square.focus();
+
+					break;
+				}
+			}
+		};
+	}
+
+	function getAdjacent(x, y) {
+		let key = `${x} ${y}`;
+		let result = adjacentMap.get(key);
+
+		if (!result) {
+			result = [
+				`${x - 1} ${y - 1}`,
+				`${x} ${y - 1}`,
+				`${x + 1} ${y - 1}`,
+				`${x - 1} ${y}`,
+				`${x + 1} ${y}`,
+				`${x - 1} ${y + 1}`,
+				`${x} ${y + 1}`,
+				`${x + 1} ${y + 1}`,
+			].reduce((results, key) => {
+				let square = boardMap.get(key);
+
+				if (square) {
+					results.push(square);
+				}
+
+				return results;
+			}, []);
+
+			adjacentMap.set(key, result);
+		}
+
+		return result;
+	}
+}
+
+function range(n) {
+	return [...Array(n).keys()];
+}
+
+export class MineSweeper extends HTMLElement {
+	constructor() {
+		super();
+
+		let width = +this.getAttribute("width");
+		let height = +this.getAttribute("height");
+		let mineCount = +this.getAttribute("mine-count");
+
+		this.style.setProperty("--width", width);
+		this.style.setProperty("--height", height);
+
+		mineSweeper({height, width, mineCount}, this);
+	}
+}
+
+customElements.define("mine-sweeper", MineSweeper);
---
+++ examples/to-do.js
@@ -0,0 +1,182 @@
+import {html, render, classes} from "../lib.js";
+
+export default function todoApp(target) {
+	let state = JSON.parse(localStorage.getItem("to-do-app")) ?? {
+		showDone: true,
+		list: [],
+	};
+	let dragItem;
+	let scheduled = false;
+
+	document.body.addEventListener("dragover", preventDragAway);
+
+	document.body.addEventListener("dragleave", preventDragAway);
+
+	document.body.addEventListener("drop", preventDragAway);
+
+	update();
+
+	function update() {
+		localStorage.setItem("to-do-app", JSON.stringify(state));
+
+		if (!scheduled) {
+			scheduled = true;
+
+			requestAnimationFrame(() => {
+				scheduled = false;
+
+				render(
+					html`<h1 class="title">To Do List</h1>
+						<input
+							class="show-done"
+							id="show-done"
+							type="checkbox"
+							checked=${state.showDone}
+							onchange=${(e) => {
+								let show = e.target.checked;
+
+								for (let item of state.list) {
+									if (item.isDone) {
+										item.isEntering = show;
+										item.isLeaving = !show;
+									}
+								}
+
+								state.showDone = show;
+
+								update();
+							}} />
+						<label for="show-done">Show done</label>
+						<input
+							class="input-text"
+							placeholder="What do you have to do?"
+							onkeypress=${(e) => {
+								if (e.keyCode === 13) {
+									e.preventDefault();
+
+									let text = e.target.value.trim();
+
+									if (!text) {
+										return;
+									}
+
+									state.list.push({
+										text,
+										isDone: false,
+										isEntering: true,
+										isLeaving: false,
+									});
+
+									e.target.value = "";
+
+									update();
+								}
+							}} />
+						<ol class="list">
+							${state.list.map((item, index) => {
+								if (!state.showDone && item.isDone && !item.isLeaving) {
+									return null;
+								}
+
+								return html`
+									<li
+										draggable="true"
+										class="item ${classes({
+											done: item.isDone,
+											leaving: item.isLeaving,
+											entering: item.isEntering,
+											dragging: dragItem === item,
+										})}"
+										ondragstart=${(e) => {
+											dragItem = item;
+
+											e.dataTransfer.effectAllowed = "move";
+											// e.dataTransfer.setDragImage(this, e.offsetX, e.offsetY);
+										}}
+										ondragend=${() => {
+											dragItem = null;
+
+											update();
+										}}
+										ondragenter=${() => {
+											if (dragItem != null) {
+												let from = state.list.findIndex((t) => t === dragItem);
+
+												state.list.splice(from, 1);
+												state.list.splice(index, 0, dragItem);
+
+												update();
+											}
+										}}
+										ondragover=${preventDefault}
+										ondragleave=${preventDefault}
+										ondrop=${preventDefault}
+										onanimationend=${() => {
+											item.isLeaving = false;
+											item.isEntering = false;
+
+											if (item.isDeleted) {
+												state.list.splice(index, 1);
+											}
+
+											update();
+										}}>
+										<input
+											type="checkbox"
+											id="${`item-${index}`}"
+											checked=${item.isDone}
+											onchange=${() => {
+												if (!state.showDone && item.isDone) {
+													item.isLeaving = true;
+												}
+
+												item.isDone = !item.isDone;
+
+												update();
+											}} />
+										<label for="${`item-${index}`}">${item.text}</label>
+										<button
+											type="button"
+											class="delete"
+											onclick=${() => {
+												item.isLeaving = true;
+												item.isDeleted = true;
+
+												update();
+											}}>
+											<svg viewBox="0 0 16 16">
+												<title>Delete</title>
+												<path
+													d="M4 1 L8 5 L12 1 L15 4 L11 8 L15 12 L12 15 L8 11 L4 15 L1 12 L5 8 L1 4 Z" />
+											</svg>
+										</button>
+									</li>
+								`;
+							})}
+						</ol>`,
+					target
+				);
+			});
+		}
+	}
+
+	function preventDefault(e) {
+		e.preventDefault();
+	}
+
+	function preventDragAway(e) {
+		if (dragItem != null) {
+			e.preventDefault();
+		}
+	}
+}
+
+export class TodoApp extends HTMLElement {
+	constructor() {
+		super();
+
+		todoApp(this);
+	}
+}
+
+customElements.define("to-do-app", TodoApp);
